37543a571823417dd4811784863f79febbc15fdf
Nurlan <101045175+NurlanShirinov@users.noreply.github.com>
2024-06-17 14:50:57 +0400

    first commot

---
+++ RabbitMq.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿//Consumerde edilmesi lazim olanlar
+//1.RabbitMQ-ya bağlantı qurulması
+//2. Baglantını aktivləşdirmək və kanal açmaq
+//3. Queue olushturmaq
+//4. Queue-dan mesaji oxumaq
+
+
+//Baglanti olushturmaq
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://mukxipkr:gYUrkZlYfZpCMrPRfKW1DXKsZ7JOG_20@shark.rmq.cloudamqp.com/mukxipkr");
+
+//Baglanti Olushturma
+using IConnection connection = factory.CreateConnection();
+
+//Kanal acmaq
+using IModel channel = connection.CreateModel();
+
+//Queue Olushturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false); // consumerde queue publisherde oldugu kimi eynisi olmalidir
+
+//Queueden Mesaj oxuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+
+consumer.Received += (sender , e)=>
+{
+    //queue gelen mesajin ishlendiyi yer.
+    //e.Body : queue daki mesjin datasini hamisini getirecekdir.
+    //e.Body.Span ve ya e.Body.ToArray() : queuedaki mesajin byte formasini getirecekdir
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿//Publisherde edilmesi lazim olanlar
+//1.RabbitMQ-ya bağlantı qurulması
+//2. Baglantını aktivləşdirmək və kanal açmaq
+//3. Queue olushturmaq
+//4. Queue ya mesaj gondermek
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+//baglanti olushturmaq
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://mukxipkr:gYUrkZlYfZpCMrPRfKW1DXKsZ7JOG_20@shark.rmq.cloudamqp.com/mukxipkr");
+
+//Baglantini aktivleshtirme
+using IConnection connection = factory.CreateConnection();
+
+//Kanal acmaq
+using IModel channel = connection.CreateModel();
+
+//Queue olushtumaq
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue-a mesaj gonderme
+//RabbitMQ queue atilan mesajlari byte tipinden qebul edir. Mesajlari byte cast etmek lazimdir.
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange:"", routingKey: "example-queue", body:message);
+
+Console.Read();
